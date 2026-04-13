c75c1b621c68d63acb0b87e0899ff3ba7beb91a2
Le Quang Tuyen <124868890+lequangtuyen543@users.noreply.github.com>
2026-04-13 10:22:46 +0700

    Deploy online

---
+++ dist/typeDefs/user.typeDefs.js
@@ -0,0 +1,29 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.typeDefsUser = void 0;
+const apollo_server_express_1 = require("apollo-server-express");
+exports.typeDefsUser = (0, apollo_server_express_1.gql) `
+  type User {
+    code: Int,
+    message: String,
+    id: String,
+    fullName: String,
+    email: String,
+    token: String
+  }
+
+  input UserInput {
+    fullName: String,
+    email: String,
+    password: String
+  }
+
+  type Query {
+    getUser: User
+  }
+
+  type Mutation {
+    registerUser(user: UserInput): User,
+    loginUser(user: UserInput): User,
+  }
+`;
